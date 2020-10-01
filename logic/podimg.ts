5966a680e405dc8a29c9c94e66cda17fb9980355
Thomas Wiegold <mail@keldrik.com>
2020-10-01 12:49:47 +1000

    imgix fit clamp

--- logic/podimg.ts
+++ logic/podimg.ts
@@ -1,1 +1,1 @@
-export const getImgUrl = (titleUrl: string) => `https://podimg.imgix.net/${titleUrl}.jpg?auto=compress,format&w=500&ar=1:1&fit=fill`
+export const getImgUrl = (titleUrl: string) => `https://podimg.imgix.net/${titleUrl}.jpg?auto=compress,format&w=500&h=500&fit=clamp`
